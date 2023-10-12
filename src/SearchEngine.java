a0824d098a91d14a296134cc75680661b8c5e393
Solaris <ovvo.system@gmail.com>
2023-10-12 20:28:43 -0300

    feat: check for valid file format

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.io.File;
 import java.util.Objects;
@@ -8,16 +9,27 @@ public class SearchEngine {
     private static final String baseDir = new File("").getAbsolutePath();
     private static final File searchDir = new File(baseDir + "/search");
     private static final List<File> searchFiles = new ArrayList<File>();
+    private static final List<String> validFormats = Arrays.asList("txt", "md");
 
     public static void main(String[] args) {
 
         if (searchDir.exists() && searchDir.isDirectory()) {
             for (File file : Objects.requireNonNull(searchDir.listFiles())) {
-                System.out.println(file);
+                if (checkForValidFileFormat(file)) {
+                    System.out.println(file);
+                }
             }
         } else {
             System.out.println("No directory found.");
         }
 
+    }
+
+    private static boolean checkForValidFileFormat(File file) {
+        int extensionIndex = file.getName().lastIndexOf(".");
+        if (extensionIndex == -1) return false;
+
+        String extension = file.getName().substring(extensionIndex + 1);
+        return validFormats.contains(extension);
     }
 }
