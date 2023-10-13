c6c5f93c029440729ea22a8d9f5866a76e3df465
Solaris <ovvo.system@gmail.com>
2023-10-13 09:55:12 -0300

    feat: create fileSearch method

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -1,14 +1,14 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.io.File;
 import java.util.Objects;
 
 public class SearchEngine {
 
     private static final String baseDir = new File("").getAbsolutePath();
     private static final File searchDir = new File(baseDir + "/search");
-    private static final List<File> searchFiles = new ArrayList<File>();
+    private static final List<File> searchFiles = new ArrayList<>();
     private static final List<String> validFormats = Arrays.asList("txt", "md");
 
     public static void main(String[] args) {
@@ -16,13 +16,17 @@ public class SearchEngine {
         if (searchDir.exists() && searchDir.isDirectory()) {
             for (File file : Objects.requireNonNull(searchDir.listFiles())) {
                 if (checkForValidFileFormat(file)) {
-                    System.out.println(file);
+                    searchFiles.add(file);
                 }
             }
         } else {
             System.out.println("No directory found.");
+            System.exit(1);
         }
 
+        for (String match : fileSearch(searchFiles, "this")) {
+            System.out.println(match);
+        }
     }
 
     private static boolean checkForValidFileFormat(File file) {
@@ -31,5 +35,29 @@ public class SearchEngine {
 
         String extension = file.getName().substring(extensionIndex + 1);
         return validFormats.contains(extension);
+    }
+
+    public static List<String> fileSearch(List<File> searchFiles, String query) {
+        List<String> searchResults = new ArrayList<>();
+        query = query.toLowerCase();
+
+        for (File file : searchFiles) {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line = reader.readLine().toLowerCase();
+                int lineNumber = 1;
+                while (line != null) {
+                    if (line.contains(query)) searchResults.add(
+                            file.getName() + " in line " + lineNumber
+                    );
+                    line = reader.readLine();
+                    lineNumber++;
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return searchResults;
     }
 }
